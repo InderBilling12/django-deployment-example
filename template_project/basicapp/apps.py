b1f9559c5e6032b444efb6d56038a02a4438844c
InderBilling12 <InderBilling12@github.com>
2018-01-10 10:01:28 +0530

    'first-commit'

---
+++ template_project/basicapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BbasicappConfig(AppConfig):
+    name = 'bbasicapp'
---
+++ template_project/basicapp/templatetags/my_extras.py
@@ -0,0 +1,8 @@
+from django import template
+register=template.Library()
+def cut(value,arg):
+    '''
+    this cuts out all the values of "arg " from the string!
+    '''
+    return value.replace(arg,'')
+register.filter('cut',cut)
